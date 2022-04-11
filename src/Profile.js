bf8e564e7d55dc04564d7afcf46fdfdec06b2b0d
rissba05 <rissba05@gmail.com>
2022-04-11 14:33:06 -0500

    created profile component and more re-formatting

---
+++ src/Profile.js
@@ -0,0 +1,63 @@
+import 
+    React
+    , { 
+        useState
+        , useEffect 
+    } from 'react';
+
+import { Auth } from 'aws-amplify';
+
+import { 
+    withAuthenticator
+    , AmplifySignOut 
+} from '@aws-amplify/ui-react/legacy';
+
+import Container from './Container';
+
+const Profile = () => {
+
+  useEffect(
+      () => {
+        checkUser()
+      }
+      , []
+  );
+
+  const [user, setUser] = useState({});
+
+  const checkUser = () => {
+    try {
+      const data = await Auth.currentUserPoolUser();
+      const userInfo = { 
+          username: data.username
+          , ...data.attributes
+      };
+
+      setUser(userInfo);
+    } 
+    
+    catch (err) {
+        console.error(err);
+    }
+  };
+
+  return (
+    <Container>
+      <h1>
+          Profile
+      </h1>
+      <h2>
+          Username: {user.username}
+      </h2>
+      <h3>
+          Email: {user.email}
+      </h3>
+      <h4>
+          Phone: {user.phone_number}
+      </h4>
+      <AmplifySignOut />
+    </Container>
+  );
+};
+
+export default withAuthenticator(Profile);
