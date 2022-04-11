30b202f90d954de299e41a6ab14d2386172fd6cd
rissba05 <rissba05@gmail.com>
2022-04-11 15:35:57 -0500

    using the latest Authenticator component from amplify ui

--- src/Profile.js
+++ src/Profile.js
@@ -7,10 +7,8 @@ import
 
 import { Auth } from 'aws-amplify';
 
-import { 
-    withAuthenticator
-    , AmplifySignOut 
-} from '@aws-amplify/ui-react/legacy';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 import Container from './Container';
 
@@ -46,7 +44,19 @@ const Profile = () => {
       <h1>
           Profile
       </h1>
-      <h2>
+
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          {console.log(user)}
+          <h2>Email: {user.attributes.email}</h2>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+
+      {/*<h2>
           Username: {user.username}
       </h2>
       <h3>
@@ -55,9 +65,9 @@ const Profile = () => {
       <h4>
           Phone: {user.phone_number}
       </h4>
-      <AmplifySignOut />
+      <AmplifySignOut /> */}
     </Container>
   );
 };
 
-export default withAuthenticator(Profile);
+export default Profile;
